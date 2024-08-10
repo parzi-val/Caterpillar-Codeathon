a6f6a26f22e1094670c0e7fcc56e61eed7d9bac3
KR Bala <krbala1511@gmail.com>
2024-08-10 12:04:07 +0530

    firebase integration

--- base/base/firebase.py
+++ base/base/firebase.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate(r"C:\Users\laksh\OneDrive\Desktop\Caterpillar\base\base\guidances-firebase-adminsdk-kt2n0-9d1a901b8d.json")
+cred = credentials.Certificate(r"C:\Users\laksh\OneDrive\Desktop\secrets\secrets.json")
 firebase_admin.initialize_app(cred)
