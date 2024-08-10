614139d41dad6d5bb5d4468eefb7a31c8293a536
Anurag Goel <gtg07817@gmail.com>
2024-08-11 04:14:25 +0530

    Home page sorted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
+'use client'
 import Navbar from "./components/Navbar";
-
+import {useRouter} from 'next/navigation'
 export default function Home() {
+  const router =useRouter();
   return (
     <main className="relative h-screen bg-cover bg-center" style={{ backgroundImage: "url('./assets/Hero.png')" }}>
       <Navbar  />
 
       <div className="flex flex-row justify-center items-center h-full">
-        <button className="bg-[#faab35] text-white px-6 py-3 m-4 rounded-md hover:bg-[#c17035]">
+        <button className="bg-[#faab35] text-white px-6 py-3 m-4 rounded-md hover:bg-[#c17035]" onClick={()=> router.push('/Manual')}>
           Manual Operation
         </button>
-        <button className="bg-[#faab35] text-white px-6 py-3 m-4 rounded-md hover:bg-[#c17035]">
+        <button className="bg-[#faab35] text-white px-6 py-3 m-4 rounded-md hover:bg-[#c17035]"onClick={()=> router.push('/Voice')}>
           Voice assistance
         </button>
       </div>
--- src/app/pages/Voice.tsx
+++ src/app/pages/Voice.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Manual = () => {
+const Voice = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Manual = () => {
   )
 }
 
-export default Manual
+export default Voice
